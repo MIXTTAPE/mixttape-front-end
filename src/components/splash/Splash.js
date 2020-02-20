4719f21f76327c58ba21eaec6115597b23167183
Ian Andrewson <ianandrewson@gmail.com>
2020-02-20 13:23:16 -0800

    Splash page component skeleton implemented

---
+++ src/components/splash/Splash.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Splash() {
+  
+  return (
+    <>
+      <h1>This is the Splash Page</h1>
+      <h2>Some info about the app will go here</h2>
+      <h3>Login and Signup components will go here</h3>
+    </>
+  );
+}
