c32b409a47b8495400349802853cc7bc5560858a
Ian Andrewson <ianandrewson@gmail.com>
2020-02-20 13:16:37 -0800

    Mixt component skeleton created

---
+++ src/components/mixt/Mixt.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function Mixt() {
+
+  return (
+    <>
+      <h1>THIS IS THE MAIN COMPONENT</h1>
+      <h2>Header goes here</h2>
+      <h2>Router goes here</h2>
+      <h3>Switch goes here</h3>
+      <h4>Splash goes here</h4>
+      <h4>Create goes here</h4>
+      <h4>TapeList goes here</h4>
+      <h4>Detail goes here</h4>
+      <h2>Player goes here</h2>
+    </>
+  );
+}
