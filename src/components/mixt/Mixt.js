50c4173869999e7e95132526821b116a3f8438e0
Ian Andrewson <ianandrewson@gmail.com>
2020-02-20 14:57:16 -0800

    implemented Player skeleton component

--- src/components/mixt/Mixt.js
+++ src/components/mixt/Mixt.js
@@ -5,6 +5,7 @@ import Create from '../create/Create.js';
 import TapeList from '../tapeList/TapeList.js';
 import TapeDetail from '../tapeDetail/TapeDetail.js';
 import AboutUs from '../aboutUs/AboutUs.js';
+import Player from '../player/Player.js';
 
 export default function Mixt() {
 
@@ -19,7 +20,7 @@ export default function Mixt() {
       <TapeList />
       <TapeDetail />
       <AboutUs />
-      <h2>Player goes here</h2>
+      <Player />
     </>
   );
 }
---
+++ src/components/player/Player.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default function Player() {
+
+  return (
+    <>
+      <h2>This is the Player section</h2>
+      <button>⏮</button>
+      <button>play/stop</button>
+      <button>⏭</button>
+      <p>it needs a display for artist and title</p>
+      <p>it needs a place for a thumbnail?</p>
+      <p>it needs a volumer control</p>
+      <p>it needs a buy link?</p>
+      <p>it needs a source logo?</p>
+      <p>it needs current time in song out of total time</p>
+      <p>it needs a current song in playlist out of total songs</p>
+    </>
+  );
+}
