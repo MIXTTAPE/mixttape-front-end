681e7a6ea65361d4b2ff00a828e2667725d5fc52
Joseph Tatum <josephtatum@gmail.com>
2020-02-21 13:41:52 -0800

    wrote setSongIndex for activeMixtapeActions and wrote passing test

--- src/actions/activeMixtapeActions.js
+++ src/actions/activeMixtapeActions.js
@@ -3,3 +3,9 @@ export const setAsActive = (payload) => ({
   type: SET_AS_ACTIVE,
   payload
 });
+
+export const SET_SONG_INDEX = 'SET_SONG_INDEX';
+export const setSongIndex = (payload) => ({
+  type: SET_SONG_INDEX,
+  payload
+});
--- src/actions/activeMixtapeActions.test.js
+++ src/actions/activeMixtapeActions.test.js
@@ -1,4 +1,4 @@
-import { setAsActive, SET_AS_ACTIVE } from './activeMixtapeActions';
+import { setAsActive, SET_AS_ACTIVE, setSongIndex, SET_SONG_INDEX } from './activeMixtapeActions';
 
 describe('activeMixtapeActions', () => {
 
@@ -23,6 +23,14 @@ describe('activeMixtapeActions', () => {
     const action = setAsActive(payload);
     expect(action).toEqual({
       type: SET_AS_ACTIVE,
+      payload
+    });
+  });
+
+  it('can set a song index', () => {
+    const action = setSongIndex(payload);
+    expect(action).toEqual({
+      type: SET_SONG_INDEX,
       payload
     });
   });
