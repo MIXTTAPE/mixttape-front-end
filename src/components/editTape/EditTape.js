590db64ea17f25699b9e27d5c1e2428119c7e7a1
Ian Andrewson <ianandrewson@gmail.com>
2020-02-21 11:28:01 -0800

    selector imported into EditTape component

--- src/components/editTape/EditTape.js
+++ src/components/editTape/EditTape.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import MixtapeSong from '../mixtapeSong/MixtapeSong.js';
+import { getLastEditedMixtape } from '../../selectors/mixtapeSelectors.js';
 
 export default function EditTape() {
-  const mixtape = useSelector();
+  const mixtape = useSelector(getLastEditedMixtape);
 
 
   //each song comes from search result section
   //Has a nativeId, native source, and title
+  
+  let mixtapeSongs;
   if(mixtape.length !== 0){
-    const mixtapeSongs = mixtape.map(song => (
+    mixtapeSongs = mixtape.map(song => (
       <li key={song.nativeId}>
         <MixtapeSong data={song} />
       </li>
@@ -21,7 +24,7 @@ export default function EditTape() {
       <h3>This is the edit MixTape section</h3>
       {/* <p>It is an unordered list of songs saved from search</p> */}
       <ul>
-
+        {mixtapeSongs}
       </ul>
       <p>each song should be reorderable within the list</p>
       <p>each song can be deleted</p>
