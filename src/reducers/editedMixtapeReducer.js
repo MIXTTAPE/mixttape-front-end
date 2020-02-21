e430d49e084cc7db4e6daf11d8ab3058ded2c554
Joseph Tatum <josephtatum@gmail.com>
2020-02-21 14:23:49 -0800

    created space for save_mixtape case. coming back to it later

--- src/reducers/editedMixtapeReducer.js
+++ src/reducers/editedMixtapeReducer.js
@@ -1,15 +1,20 @@
-import { ADD_SONG, DELETE_SONG, SET_AS_EDITED } from '../actions/editedMixtapeActions';
+import { ADD_SONG, DELETE_SONG, SET_AS_EDITED, SAVE_MIXTAPE } from '../actions/editedMixtapeActions';
 
 const initialState = {};
 
 export const editedMixtapeReducer = (state = initialState, action) => {
   switch(action.type) {
-    case SET_AS_EDITED:
-      return action.payload;
     case ADD_SONG:
       return { ...state, songs: [...state.songs, action.payload] };
     case DELETE_SONG:
       return { ...state, songs: state.songs.filter(item => item.nativeId !== action.payload.nativeId) };
+    case SET_AS_EDITED:
+      return action.payload;
+    case SAVE_MIXTAPE:
+      /*I AM NOT WORKING YET!!!! */
+      /* send mixtape to the database */
+      /* add mixtape to state */
+      return {};
     default:
       return state;
   }
