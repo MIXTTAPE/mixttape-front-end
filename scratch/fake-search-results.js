2626353ffb255425982423516d5489c1b08de51b
Ian Andrewson <ianandrewson@gmail.com>
2020-02-20 18:40:22 -0800

    youtube results render to result section

---
+++ scratch/fake-search-results.js
@@ -0,0 +1,172 @@
+/* eslint-disable quotes */
+export const fakeSearchResults = {
+  kind: "youtube#searchListResponse",
+  etag: "\"Fznwjl6JEQdo1MGvHOGaz_YanRU/V56sTKoMDp8yx0YAlURuSAqshgg\"",
+  nextPageToken: "CAUQAA",
+  regionCode: "US",
+  pageInfo: {
+    totalResults: 1000000,
+    resultsPerPage: 5
+  },
+  items: [
+    {
+      kind: "youtube#searchResult",
+      etag: "\"Fznwjl6JEQdo1MGvHOGaz_YanRU/YCed2NvZsJ5uIMb5W60VYK9ZABo\"",
+      id: {
+        kind: "youtube#video",
+        videoId: "CcNo07Xp8aQ"
+      },
+      snippet: {
+        publishedAt: "2010-05-28T18:10:03.000Z",
+        channelId: "UCX6kV7z1OW-x24NwvXzyY5Q",
+        title: "Robyn - Dancing On My Own (Official Video)",
+        description: "Follow & Hear More From Robyn : https://rbn.lnk.to/FollowYD Music video by Robyn performing Dancing On My Own. (C) 2010 Konichiwa / Cherrytree ...",
+        thumbnails: {
+          default: {
+            url: "https://i.ytimg.com/vi/CcNo07Xp8aQ/default.jpg",
+            width: 120,
+            height: 90
+          },
+          medium: {
+            url: "https://i.ytimg.com/vi/CcNo07Xp8aQ/mqdefault.jpg",
+            width: 320,
+            height: 180
+          },
+          high: {
+            url: "https://i.ytimg.com/vi/CcNo07Xp8aQ/hqdefault.jpg",
+            width: 480,
+            height: 360
+          }
+        },
+        channelTitle: "RobynVEVO",
+        liveBroadcastContent: "none"
+      }
+    },
+    {
+      kind: "youtube#searchResult",
+      etag: "\"Fznwjl6JEQdo1MGvHOGaz_YanRU/zDUcJxYZODiXS0lelPKuT7XpXTg\"",
+      id: {
+        kind: "youtube#channel",
+        channelId: "UCC_JSfQlhrKeA_jNeqhLAiA"
+      },
+      snippet: {
+        publishedAt: "2007-05-17T10:11:30.000Z",
+        channelId: "UCC_JSfQlhrKeA_jNeqhLAiA",
+        title: "Robyn",
+        description: "Robyn's album Honey is out now: https://rbn.lnk.to/HONEYALBUMYD Pre-save Missing U here: https://rbn.lnk.to/MissingUYo Follow & Hear More From Robyn ...",
+        thumbnails: {
+          default: {
+            url: "https://yt3.ggpht.com/-Nq1SiM0fFuM/AAAAAAAAAAI/AAAAAAAAAAA/sktl5KspSYM/s88-c-k-no-mo-rj-c0xffffff/photo.jpg"
+          },
+          medium: {
+            url: "https://yt3.ggpht.com/-Nq1SiM0fFuM/AAAAAAAAAAI/AAAAAAAAAAA/sktl5KspSYM/s240-c-k-no-mo-rj-c0xffffff/photo.jpg"
+          },
+          high: {
+            url: "https://yt3.ggpht.com/-Nq1SiM0fFuM/AAAAAAAAAAI/AAAAAAAAAAA/sktl5KspSYM/s800-c-k-no-mo-rj-c0xffffff/photo.jpg"
+          }
+        },
+        channelTitle: "Robyn",
+        liveBroadcastContent: "upcoming"
+      }
+    },
+    {
+      kind: "youtube#searchResult",
+      etag: "\"Fznwjl6JEQdo1MGvHOGaz_YanRU/z29APilfdWUtoaa3YtneEsv9SZw\"",
+      id: {
+        kind: "youtube#video",
+        videoId: "F6ImxY6hnfA"
+      },
+      snippet: {
+        publishedAt: "2011-06-03T16:30:05.000Z",
+        channelId: "UCX6kV7z1OW-x24NwvXzyY5Q",
+        title: "Robyn - Call Your Girlfriend (Official Video)",
+        description: "Follow & Hear More From Robyn : https://rbn.lnk.to/FollowYD Music video by Robyn performing Call Your Girlfriend. (C) 2011 Konichiwa Records, under ...",
+        thumbnails: {
+          default: {
+            url: "https://i.ytimg.com/vi/F6ImxY6hnfA/default.jpg",
+            width: 120,
+            height: 90
+          },
+          medium: {
+            url: "https://i.ytimg.com/vi/F6ImxY6hnfA/mqdefault.jpg",
+            width: 320,
+            height: 180
+          },
+          high: {
+            url: "https://i.ytimg.com/vi/F6ImxY6hnfA/hqdefault.jpg",
+            width: 480,
+            height: 360
+          }
+        },
+        channelTitle: "RobynVEVO",
+        liveBroadcastContent: "none"
+      }
+    },
+    {
+      kind: "youtube#searchResult",
+      etag: "\"Fznwjl6JEQdo1MGvHOGaz_YanRU/835wW4qkRMvt2S6Jfinz5eG7184\"",
+      id: {
+        kind: "youtube#video",
+        videoId: "GfK88LsB0fg"
+      },
+      snippet: {
+        publishedAt: "2019-06-17T13:00:03.000Z",
+        channelId: "UCX6kV7z1OW-x24NwvXzyY5Q",
+        title: "Robyn - Ever Again",
+        description: "Music video by Robyn performing Ever Again. © 2019 Konichiwa Records, under exclusive licence to Universal Music Operations Limited Ever Again is taken ...",
+        thumbnails: {
+          default: {
+            url: "https://i.ytimg.com/vi/GfK88LsB0fg/default.jpg",
+            width: 120,
+            height: 90
+          },
+          medium: {
+            url: "https://i.ytimg.com/vi/GfK88LsB0fg/mqdefault.jpg",
+            width: 320,
+            height: 180
+          },
+          high: {
+            url: "https://i.ytimg.com/vi/GfK88LsB0fg/hqdefault.jpg",
+            width: 480,
+            height: 360
+          }
+        },
+        channelTitle: "RobynVEVO",
+        liveBroadcastContent: "none"
+      }
+    },
+    {
+      kind: "youtube#searchResult",
+      etag: "\"Fznwjl6JEQdo1MGvHOGaz_YanRU/vD-UXPfXBZ4Hcpd1ZEBKNn80EQI\"",
+      id: {
+        kind: "youtube#video",
+        videoId: "bhWEI6-_w9E"
+      },
+      snippet: {
+        publishedAt: "2009-11-26T04:32:04.000Z",
+        channelId: "UCX6kV7z1OW-x24NwvXzyY5Q",
+        title: "Robyn - Show Me Love (Official Video)",
+        description: "Music video by Robyn Carlsson performing Show Me Love. (C) 1997 Ricochet/BMG Sweden AB #Robyn #ShowMeLove #Vevo.",
+        thumbnails: {
+          default: {
+            url: "https://i.ytimg.com/vi/bhWEI6-_w9E/default.jpg",
+            width: 120,
+            height: 90
+          },
+          medium: {
+            url: "https://i.ytimg.com/vi/bhWEI6-_w9E/mqdefault.jpg",
+            width: 320,
+            height: 180
+          },
+          high: {
+            url: "https://i.ytimg.com/vi/bhWEI6-_w9E/hqdefault.jpg",
+            width: 480,
+            height: 360
+          }
+        },
+        channelTitle: "RobynVEVO",
+        liveBroadcastContent: "none"
+      }
+    }
+  ]
+};
