ef9eba71bfd47b64abf58268ef3af367816083b8
Ian Andrewson <ianandrewson@gmail.com>
2020-02-21 09:54:19 -0800

    Edit Tape component expanded. Mixtape taken from state via selector. Mixtape mapped over to create song components for list. Ready to make song component

--- src/components/editTape/EditTape.js
+++ src/components/editTape/EditTape.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import MixtapeSong from '../mixtapeSong/MixtapeSong.js';
 
 export default function EditTape() {
+  const mixtape = useSelector();
+
+
+  //each song comes from search result section
+  //Has a nativeId, native source, and title
+  if(mixTape.length !== 0){
+    const mixtapeSongs = mixtape.map(song => (
+      <li key={song.nativeId}>
+        <MixtapeSong data={song} />
+      </li>
+    ));
+  }
 
   return (
     <>
       <h3>This is the edit MixTape section</h3>
-      <p>It is an unordered list of songs saved from search</p>
+      {/* <p>It is an unordered list of songs saved from search</p> */}
+      <ul>
+
+      </ul>
       <p>each song should be reorderable within the list</p>
       <p>each song can be deleted</p>
       <button>Save</button>
