9087f940c5bdf6d56494897ad86cda7a0257c716
Joseph Tatum <josephtatum@gmail.com>
2020-02-20 17:26:27 -0800

    removing unnecessary files

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,6 @@
 import React from 'react';
-import youtube from '../services/youtube';
 
 export default function App() {
-  youtube('malamente')
-    .then(res => console.log(res.items));
-
   return <h1>Hello World</h1>;
 }
+ 
