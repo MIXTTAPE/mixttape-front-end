e552fbcdca60b95b3589334f0c9fa2fec7d4734c
Ian Andrewson <ianandrewson@gmail.com>
2020-02-21 19:12:30 -0800

    editedMixtapeReducer initial state fixed. Add song to edit list action implemented; songs add to lastEditedMixtape.songs state correctly.

--- src/components/searchResultSectionItem/SearchResultSectionItem.js
+++ src/components/searchResultSectionItem/SearchResultSectionItem.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import PropTypes, { string, number } from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { addSong } from '../../actions/editedMixtapeActions';
 
 export default function SearchResultSectionItem({ data }) {
+  const dispatch = useDispatch();
+
+  const handleAdd = () => {
+    dispatch(addSong(data));
+  };
 
   return (
     <span>
       {data.title}
-      <button>+</button>
+      <button onClick={handleAdd}>+</button>
       <button>Demo</button>
     </span>
   );
