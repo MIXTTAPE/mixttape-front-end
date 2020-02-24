2b3fa2590a255e70e6b2d3dcecaf88de9b54fcad
Joseph Tatum <josephtatum@gmail.com>
2020-02-24 11:29:12 -0800

    set up mixit router to render specific section based on url

--- src/components/mixt/Mixt.js
+++ src/components/mixt/Mixt.js
@@ -1,23 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from '../header/Header.js';
-import Splash from '../splash/Splash.js';
 import Create from '../create/Create.js';
 import TapeList from '../tapeList/TapeList.js';
 import TapeDetail from '../tapeDetail/TapeDetail.js';
-import AboutUs from '../aboutUs/AboutUs.js';
 import Player from '../player/Player.js';
 import '../../styles/global.css';
 
 export default function Mixt() {
 
   return (
-    <>
+    <Router>
       <Header />   
-      <Create />
-      <TapeList />
-      <TapeDetail />
-      <AboutUs />
+      <Route path="/app/create" component={Create} />
+      <Route path="/app/my-mixtapes" component={TapeList} />
+      <Route path="/app/mixtape/:id" component={TapeDetail} />
       <Player />
-    </>
+    </Router>
   );
 }
