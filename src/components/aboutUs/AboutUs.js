3f3e903e105f7065a8ba07ee3bec479ed1e6d7ac
Joseph Tatum <josephtatum@gmail.com>
2020-02-24 14:19:40 -0800

    updated aboutus.js file with real content

--- src/components/aboutUs/AboutUs.js
+++ src/components/aboutUs/AboutUs.js
@@ -4,8 +4,13 @@ export default function AboutUs() {
 
   return (
     <>
-      <h2>This is the about us section</h2>
-      <p>it will be some information about us</p>
+      <h1>The Team</h1>
+      <ul>
+        <li>Matt Munch</li>
+        <li>Joseph Tatum</li>
+        <li>Travis Molter</li>
+        <li>Ian Andrewson</li>
+      </ul>
     </>
   );
 }
