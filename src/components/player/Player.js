4f12e09bf115a8d113e97fbd3430473f470087b1
Joseph Tatum <josephtatum@gmail.com>
2020-02-24 10:04:59 -0800

    created player.css and styled out the footer component

--- src/components/player/Player.js
+++ src/components/player/Player.js
@@ -1,20 +1,31 @@
 import React from 'react';
+import '../../styles/player.css';
 
 export default function Player() {
 
   return (
-    <>
-      <h2>This is the Player section</h2>
-      <button>⏮</button>
-      <button>play/stop</button>
-      <button>⏭</button>
-      <p>it needs a display for artist and title</p>
+    <footer className="player-component">
+      <div className="player-container">
+        <div className="currently-playing">
+          <img src="thu" />
+          <p className="track-title">Track Title</p>
+        </div>
+        <div className="player-controls">
+          <button>play/stop</button>
+          <button>⏭</button>
+        </div>
+        <div className="volume-controls">
+          <input type="range" id="volume" name="volume"
+            min="0" max="11" />
+        </div>
+      </div>
+      {/* <p>it needs a display for artist and title</p>
       <p>it needs a place for a thumbnail?</p>
       <p>it needs a volumer control</p>
       <p>it needs a buy link?</p>
       <p>it needs a source logo?</p>
       <p>it needs current time in song out of total time</p>
-      <p>it needs a current song in playlist out of total songs</p>
-    </>
+      <p>it needs a current song in playlist out of total songs</p> */}
+    </footer>
   );
 }
