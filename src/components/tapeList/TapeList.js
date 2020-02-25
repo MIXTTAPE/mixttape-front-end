98c1269fcabd6e973f51fd2d6ae5c18e67ce8d50
Travis Molter <travismolter@gmail.com>
2020-02-25 11:44:55 -0800

    created the mixtape card elements to list out mixtape title, created by and the song thumbnail

--- src/components/tapeList/TapeList.js
+++ src/components/tapeList/TapeList.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import { mixtapes } from './fakeMixtapes';
 
 export default function TapeList() {
+  const songElements = mixtapes.map((mixtape, i) => {
+    return mixtape.songs.map((song, j) => {
+      return (
+        <li key={j} >
+          <p>{song.thumbnailUrl}</p>
+        </li>
+      );
+    });
+  });
+
+  const mixtapeCards = mixtapes.map((mixtape, i) => (
+    <li key={i}>
+      <h2>Mixtape Name:{mixtape.mixtapeName}</h2>
+      <h3>Created By: {mixtape.createdBy}</h3>
+      <ul>
+        {songElements[i]}
+      </ul>
+
+    </li>
+  ));
 
   return (
     <>
       <ul>
+        {mixtapeCards}
         <h2>This is the TapeList</h2>
         <p>Each list item is a mixtape</p>
         <p>Clicking an item should redirect to its detail page</p>
---
+++ src/components/tapeList/fakeMixtapes.js
@@ -0,0 +1,60 @@
+export const mixtapes = [{
+  userId: '12345',
+  mixtapeName: 'The Best One',
+  createdBy: 'treemo',
+  songs: [
+    {
+      nativeId: '345',
+      nativeSource: 'soundcloud',
+      title: 'Chris Stapleton - Traveler',
+      thumbnailUrl: 'chrisstapletontraveler.jpg',
+      buyLink: 'buythis.com',
+      isMemo: false,
+      tags: ['country']
+    },
+    {
+      nativeId: '3684',
+      nativeSource: 'soundcloud',
+      title: 'Chris Stapleton - Tenesse Whiskey',
+      thumbnailUrl: 'chrisstapletontenessewhiskey.jpg',
+      buyLink: 'buythis.com',
+      isMemo: false,
+      tags: ['country']
+    },
+    {
+      nativeId: '367843',
+      nativeSource: 'soundcloud',
+      title: 'Zak Brown Band - Toes',
+      thumbnailUrl: 'zakbrownbandtoes.jpg',
+      buyLink: 'buythistune.com',
+      isMemo: false,
+      tags: ['country']
+    }      
+  ],
+  rating: 2,
+}, {
+  userId: '34567',
+  mixtapeName: 'The Better One',
+  createdBy: 'treemo',
+  songs: [
+    {
+      nativeId: '567',
+      nativeSource: 'youtube',
+      title: 'Lady Gaga - Shallow',
+      thumbnailUrl: 'ladygagashallow.jpg',
+      buyLink: 'buythissicktrack.com',
+      isMemo: false,
+      tags: ['pop']
+    },
+    {
+      nativeId: '788',
+      nativeSource: 'soundcloud',
+      title: 'Leon Bridges - Shy',
+      thumbnailUrl: 'leonbridgesshy.jpg',
+      buyLink: 'buythissicktrack.com',
+      isMemo: false,
+      tags: ['pop']
+    }   
+  ],
+  rating: 3
+}];
