1cb3241052f79646e804fa2001dab26358d8d13c
Joseph Tatum <josephtatum@gmail.com>
2020-02-24 16:10:33 -0800

    commented out uncessary html on editTape.js file and updated text

--- src/components/editTape/EditTape.js
+++ src/components/editTape/EditTape.js
@@ -6,8 +6,6 @@ import { getLastEditedMixtape } from '../../selectors/editedMixtapeSelectors.js'
 export default function EditTape() {
   const mixtape = useSelector(getLastEditedMixtape);
 
-  
-
   //each song comes from search result section
   //Has a nativeId, native source, and title
   
@@ -31,16 +29,16 @@ export default function EditTape() {
 
   return (
     <>
-      <h3>This is the edit MixTape section</h3>
+      <h2>Mixtape Title</h2>
       {/* <p>It is an unordered list of songs saved from search</p> */}
       <ul>
         {mixtapeSongs}
       </ul>
-      <p>each song should be reorderable within the list</p>
-      <p>each song can be deleted</p>
-      <button>Save</button>
-      <p>Save button should post playlist to DB</p>
-      <p>Save button should redirect to the mixtape detail page</p>
+      {/* <p>each song should be reorderable within the list</p>
+      <p>each song can be deleted</p> */}
+      <button className="button-primary">Save</button>
+      {/* <p>Save button should post playlist to DB</p>
+      <p>Save button should redirect to the mixtape detail page</p> */}
     </>
   );
 }
