09979c77a79903c1699f838a6f6d31300edd0c6f
Joseph Tatum <josephtatum@gmail.com>
2020-02-25 18:54:23 -0800

    restructuring some of Travis work in prep for styling

--- src/components/tapeList/TapeList.js
+++ src/components/tapeList/TapeList.js
@@ -1,48 +1,31 @@
 import React from 'react';
-// import { mixtapes } from './fakeMixtapes';
+import { mixtapes } from './fakeMixtapes';
+import { useSelector } from 'react-redux';
 import { getUserMixtapes } from '../../selectors/userSelectors';
 
 export default function TapeList() {
-  const mixtapes = getUserMixtapes();
-  if(mixtapes === undefined) {
-    const noMix = {};
-    return (
-      <h1>User has no Mixtapes</h1>
-    );
-  } else {
-    const songElements = mixtapes.map(mixtape => {
-      return mixtape.songs.map((song, j) => {
-        return (
-          <li key={j} >
-            <img src={song.thumbnailUrl} placeholder='Song'></img>
-          </li>
-        );
-      });
-    });
 
-    const mixtapeCards = mixtapes.map((mixtape, i) => (
-      <li key={i}>
-        <h2>Mixtape Name:{mixtape.mixtapeName}</h2>
-        <h3>Created By: {mixtape.createdBy}</h3>
-        <ul>
-          {songElements[i]}
-        </ul>
-        <button>Play</button>
-        <p>play button should add playlist to state and begin playing first song</p>
-        <button>Edit</button>
-        <p>edit button sould redirect to the create page with selected playlist loaded ready for editing</p>
-      </li>
-    ));
-  
+  // const mixtapes = useSelector(getUserMixtapes);
+
+  if(mixtapes.length === 0) {
     return (
-      <>
-        <ul>
-          {mixtapeCards}
-          <h2>This is the TapeList</h2>
-          <p>Each list item is a mixtape</p>
-          <p>Clicking an item should redirect to its detail page</p>
-        </ul>
-      </>
+      <h2>Hmm.... looks like you don`&apos;`t have any mixtapes. Create one?</h2>
     );
   }
+
+  const mixtapeCards = mixtapes.map(mixtape => {
+    return (
+      <li key={mixtape._id}>
+        <
+      </li>
+    );
+  });
+
+  return (
+    <>
+      <ul>
+        <li></li>
+      </ul>
+    </>
+  );
 }
