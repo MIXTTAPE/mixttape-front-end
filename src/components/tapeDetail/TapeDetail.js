7151c69887cdf80822fba1502c38e6eb7cd278a9
Joseph Tatum <josephtatum@gmail.com>
2020-02-25 16:56:34 -0800

    a bit more

--- src/components/tapeDetail/TapeDetail.js
+++ src/components/tapeDetail/TapeDetail.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
 import { setPlaying } from '../../actions/activeMixtapeActions';
 import { getPlaying } from '../../selectors/activeMixtapeSelectors';
 import { fakeMixtape as activeMixtape } from '../../../scratch/fake-mixtape';
 import { FaPlayCircle, FaPauseCircle } from 'react-icons/fa';
 
 export default function TapeDetail() {
+  
+  const { id } = useParams();
+
+  useEffect(() => {
+    // on load make a fetch call to render the correct playlist
+  });
 
   const dispatch = useDispatch();
   const playing = useSelector(getPlaying);
@@ -33,16 +40,3 @@ export default function TapeDetail() {
     </div>
   );
 }
-
-{/* <ul>
-  <h2>This is the TapeDetail Page</h2>
-  <p>Each item is a song</p>
-</ul>
-  <h3>There should be a section for displaying ratings</h3>
-  <p>There should be a way to rate a mixtape</p>
-  <p>There should be a list of tags?</p>
-  <p>There should be a way to add tags without having to edit the mixtape?</p>
-  <button>Share</button>
-  <p>The share button should generate a link to share with  mixtape</p>
-  <button>Play</button>
-  <p>The play button sould play the playlist from the beginning</p> */}
