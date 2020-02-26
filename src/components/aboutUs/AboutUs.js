f33e3cfd895508e73f247443d5272ef0a7a04a7e
Joseph Tatum <josephtatum@gmail.com>
2020-02-26 14:03:16 -0800

    filled out basic info on about us page

--- src/components/aboutUs/AboutUs.js
+++ src/components/aboutUs/AboutUs.js
@@ -1,16 +1,37 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Splash from '../splash/Splash';
+import { FaLinkedinIn, FaGithub } from 'react-icons/fa';
+
 export default function AboutUs() {
 
   return (
     <>
       <h1>The Team</h1>
       <ul>
-        <li>Matt Munch</li>
-        <li>Joseph Tatum</li>
-        <li>Travis Molter</li>
-        <li>Ian Andrewson</li>
+        <li>
+          <h2>Matt Munch</h2>
+          <p>About Matt</p>
+          <a href="https://www.linkedin.com/in/mattmunch/" target="_brel=" rel="noopener noreferrer"><FaLinkedinIn className="social-icon" /></a>
+          <a href="https://github.com/Mattmunch" target="_brel=" rel="noopener noreferrer"><FaGithub className="social-icon" /></a>
+        </li>
+        <li>
+          <h2>Joseph Tatum</h2>
+          <p>About Joseph</p>
+          <a href="https://www.linkedin.com/in/josephwtatum/" target="_brel=" rel="noopener noreferrer"><FaLinkedinIn className="social-icon" /></a>
+          <a href="https://github.com/josephtatum" target="_brel=" rel="noopener noreferrer"><FaGithub className="social-icon" /></a>
+        </li>
+        <li>
+          <h2>Travis Molter</h2>
+          <p>About Travis</p>
+          <a href="https://www.linkedin.com/in/travismolter/" target="_brel=" rel="noopener noreferrer"><FaLinkedinIn className="social-icon" /></a>
+          <a href="https://github.com/treem0" target="_brel=" rel="noopener noreferrer"><FaGithub className="social-icon" /></a>
+        </li>
+        <li>
+          <h2>Ian Andrewson</h2>
+          <p>About Ian</p>
+          <a href="https://www.linkedin.com/in/ianandrewson/" target="_brel=" rel="noopener noreferrer"><FaLinkedinIn className="social-icon" /></a>
+          <a href="https://github.com/ianandrewson" target="_brel=" rel="noopener noreferrer"><FaGithub className="social-icon" /></a>
+        </li>
       </ul>
       <Link to="/">Go Back </Link>
     </>
