b4fcae0a9624e5d35a247bf37438834c917035cd
Joseph Tatum <josephtatum@gmail.com>
2020-02-26 19:11:27 -0800

    logout is functional

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -4,12 +4,17 @@ import { logout } from '../../services/auth';
 
 export default function Nav() {
 
+  const handleLogout = () => {
+    logout()
+      .then(window.location.href = ('/'));
+  };
+
   return (
     <nav>
       <ul>
         <li><Link to={'/app/create'}>Create</Link></li>
         <li><Link to={'/app/mixtapes'}>My Mixtapes</Link></li>
-        <li><Link to={'/'}>Logout</Link></li>
+        <li onClick={handleLogout}>Logout</li>
       </ul>
     </nav>
   );
