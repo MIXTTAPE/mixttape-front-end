8b0c6812dfd442c76dcf31427bf139dce02e6178
Joseph Tatum <josephtatum@gmail.com>
2020-02-27 09:49:41 -0800

    logout button hover, styling record another button, removing pause button from mixtape list view

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, useHistory, Redirect } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { logout } from '../../services/auth';
 
 export default function Nav() {
@@ -14,7 +14,7 @@ export default function Nav() {
       <ul>
         <li><Link to={'/app/create'}>Create</Link></li>
         <li><Link to={'/app/mixtapes'}>My Mixtapes</Link></li>
-        <li onClick={handleLogout}>Logout</li>
+        <li><Link to={'/'} onClick={handleLogout}>Logout</Link></li>
       </ul>
     </nav>
   );
