0984cafb79039a8d34731cb30e231758d3340fb5
Joseph Tatum <josephtatum@gmail.com>
2020-04-01 16:21:45 -0700

    removing more unnecessary code comments

--- src/services/masterApiCall.js
+++ src/services/masterApiCall.js
@@ -3,8 +3,6 @@ import youtubeApi from './youtube.js';
 
 export default function masterApiCall(query) {
   return Promise.all([
-    //soundcloud needs a custom searchResultSection component
-    //commented out until that happens.
     soundcloudApi(query),
     youtubeApi(query)
   ]);
